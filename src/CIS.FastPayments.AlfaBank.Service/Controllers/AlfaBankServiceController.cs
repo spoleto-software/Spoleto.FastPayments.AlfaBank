fb122f7fcc24e394145fff82194082c3e89118c2
Oleg Nadymov <OlegNadymov@gmail.com>
2022-10-05 00:56:27 +0300

    Draft for CIS.FastPayments.AlfaBank.Service

---
+++ src/CIS.FastPayments.AlfaBank.Service/Controllers/AlfaBankServiceController.cs
@@ -0,0 +1,35 @@
+﻿using CIS.FastPayments.AlfaBank.Service.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS.FastPayments.AlfaBank.Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlfaBankServiceController : ControllerBase
+    {
+        private readonly ILogger<AlfaBankServiceController> _logger;
+
+        public AlfaBankServiceController(ILogger<AlfaBankServiceController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        //[Consumes(DefaultSettings.ContentType)]
+        public async Task<QRCodeReport> CreateQRCodeReport(QRCodeReport reportModel)
+        {
+            _logger.LogInformation($"{nameof(CreateQRCodeReport)}, QrcId = <{reportModel.QrcId}>.");
+            //var fiscalRequest = new FiscalRequest
+            //{
+            //    SaleSlipId = Guid.Parse(reportModel.ExternalId),
+            //    Uuid = reportModel.Uuid,
+            //    Timestamp = DateTime.UtcNow,
+            //    OriginalReportModel = reportModel
+            //};
+
+            //await _fiscalRequestService.CreateAsync(fiscalRequest);
+
+            return reportModel;
+        }
+    }
+}
