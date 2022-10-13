82f8e3c5df938363aa22522d8c97481e30c44e55
Oleg Nadymov <OlegNadymov@gmail.com>
2022-10-13 23:43:14 +0300

    test

--- src/Spoleto.FastPayments.AlfaBank.Tests/AlfaProviderTests.cs
+++ src/Spoleto.FastPayments.AlfaBank.Tests/AlfaProviderTests.cs
@@ -6,6 +6,9 @@ using Spoleto.FastPayments.AlfaBank.Providers;
 
 namespace Spoleto.FastPayments.AlfaBank.Tests
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class AlfaProviderTests
     {
         private ServiceProvider _serviceProvider;
